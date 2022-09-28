76827b773483d657641b640d9a2ddc27c4c7b0cf
Ankit Raj <ankittrajj1996@gmail.com>
2022-09-28 12:56:55 +0530

    Add files via upload

---
+++ databse.py
@@ -0,0 +1,24 @@
+import mysql.connector as c
+con = c.connect(host = 'localhost',
+                user = 'root',
+                passwd = 'Ankit@8285',
+                database ='ironman')
+
+
+cursor = con.cursor()
+
+# if con.is_connected():
+#     print("Connection Successfully!!")
+while True:
+    name = input("Enter the name")
+    age = int(input("Enter the age"))
+    marks = int(input('Enter the marks'))
+
+    query = "Insert into jarvis values('{}',{},{})".format(name,age,marks)
+    cursor.execute(query)
+    con.commit()
+
+    choice = 2
+    if choice == 2:
+        break
+    print("Data entered successfully!!")
